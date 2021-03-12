a58e364c3ca0690bf54c1a4e580f9c03adb5e573
lucasryg <lucasryg@gmail.com>
2021-03-12 11:40:54 -0300

    GOGO-10: Criando o ambiente

---
+++ LucasGoncalves/mobileApp/mobileApp/mobileApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using mobileApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace mobileApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ LucasGoncalves/mobileApp/mobileApp/mobileApp/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using mobileApp.Models;
+using mobileApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace mobileApp.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
