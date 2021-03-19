805d70967786e7ebf049d5a02cb6ec2602249e7d
Rodhenrique <henriquesilvasenai@gmail.com>
2021-03-19 17:04:04 -0300

    GOGO-9: Agora vai

---
+++ Henrique H. Silva/Goiabeira_Xamarin_Mobile/Goiabeira_Xamarin_Mobile/obj/Debug/netstandard2.0/Goiabeira_Xamarin_Mobile.AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Reflection;
+
+[assembly: System.Reflection.AssemblyCompanyAttribute("Goiabeira_Xamarin_Mobile")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyProductAttribute("Goiabeira_Xamarin_Mobile")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Goiabeira_Xamarin_Mobile")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+
