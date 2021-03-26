32a8668c95da89edfadd156376d2dfc6e01b855d
lucasryg <lucasryg@gmail.com>
2021-03-26 13:00:15 -0300

    GUIRELLI-3: Preparação de ambiente feita, cfg file criado

---
+++ LearningFix/LearningFix/AcceptorClass.cs
@@ -0,0 +1,20 @@
+﻿using QuickFix;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningFix
+{
+    class AcceptorClass : IApplication
+    {
+        public void FromApp(Message msg, SessionID sessionID) { }
+        public void OnCreate(SessionID sessionID) { }
+        public void OnLogout(SessionID sessionID) { }
+        public void OnLogon(SessionID sessionID) { }
+        public void FromAdmin(Message msg, SessionID sessionID) { }
+        public void ToAdmin(Message msg, SessionID sessionID) { }
+        public void ToApp(Message msg, SessionID sessionID) { }
+    }
+}
---
+++ LearningFix/LearningFix/Program.cs
@@ -0,0 +1,40 @@
+﻿using QuickFix;
+using System;
+
+namespace LearningFix
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("usage: SimpleAcceptor CONFIG_FILENAME");
+                System.Environment.Exit(2);
+            }
+
+            try
+            {
+                SessionSettings settings = new SessionSettings(args[0]);
+                IApplication app = new AcceptorClass();
+                IMessageStoreFactory storeFactory = new FileStoreFactory(settings);
+                ILogFactory logFactory = new FileLogFactory(settings);
+                IAcceptor acceptor = new ThreadedSocketAcceptor(app, storeFactory, settings, logFactory);
+
+                acceptor.Start();
+                while (true)
+                {
+                    System.Console.WriteLine("IN");
+                    Console.ReadLine();
+                }
+                acceptor.Stop();
+            }
+            catch (System.Exception e)
+            {
+                Console.WriteLine("==FATAL ERROR==");
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
