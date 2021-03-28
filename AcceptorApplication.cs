278270539cece7c63b8244c852c5a94b6efedf09
tasawe <watasejoao@gmail.com>
2021-03-28 18:43:27 -0300

    GUIRELLI-3: Adiciona o app basico com arquivo de configuração

---
+++ AcceptorApplication.cs
@@ -0,0 +1,32 @@
+﻿using QuickFix;
+using System;
+
+namespace QuickfixAcceptor
+{
+    public class AcceptorApplication : IApplication
+    {
+        public void FromApp(Message message, SessionID sessionID)
+        {
+            Console.WriteLine("IN:  " + message);
+        }
+
+        public void ToApp(Message message, SessionID sessionID)
+        {
+            Console.WriteLine("OUT: " + message);
+        }
+
+        public void FromAdmin(Message message, SessionID sessionID)
+        {
+            Console.WriteLine("IN:  " + message);
+        }
+
+        public void ToAdmin(Message message, SessionID sessionID)
+        {
+            Console.WriteLine("OUT:  " + message);
+        }
+
+        public void OnCreate(SessionID sessionID) { }
+        public void OnLogout(SessionID sessionID) { }
+        public void OnLogon(SessionID sessionID) { }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using QuickFix;
+using System;
+using System.IO;
+
+namespace QuickfixAcceptor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader file = File.OpenText("SessionSettings.cfg");
+            SessionSettings settings = new SessionSettings(file);
+            IApplication app = new AcceptorApplication();
+            IMessageStoreFactory storeFactory = new FileStoreFactory(settings);
+            ILogFactory logFactory = new FileLogFactory(settings);
+            IAcceptor acceptor = new ThreadedSocketAcceptor(app, storeFactory, settings, logFactory);
+            acceptor.Start();
+            Console.WriteLine("press <enter> to quit");
+            Console.Read();
+            acceptor.Stop();
+        }
+    }
+}
